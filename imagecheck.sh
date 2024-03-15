28092883ed272e12532ef386a4806a36de1ebe7c
cooloo9871 <62133915+cooloo9871@users.noreply.github.com>
2024-03-15 15:23:30 +0800

    Update imagecheck.sh

--- imagecheck.sh
+++ imagecheck.sh
@@ -2,6 +2,7 @@
 #set -x
 
 NODENAME=$(kubectl get no -o name | cut -d '/' -f2)
+[[ -f image.txt ]] && rm image.txt
 
 for nn in $NODENAME
 do
@@ -12,7 +13,7 @@ do
     pod=$(kubectl get pods -n $c -o wide | grep -w $nn | tr -s \ -| cut -d ' ' -f1)
     for n in $pod
     do
-      kubectl -n $c get pod $n -o jsonpath='{.spec.containers[*].image}' >> image.txt
+      kubectl -n $c get pod $n -o jsonpath='{.spec.containers[*].image}' | tr ' ' '\n' >> image.txt
       echo -e "\n" >> image.txt
     done
   done
